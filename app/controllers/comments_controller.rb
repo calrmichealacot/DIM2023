7f43676a2a6f4519754191ce1f1c3b408efacedf
Calr Micheal Acot <calrmichealacot@gmail.com>
2023-04-11 02:11:18 +0800

    fix the comment post and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,9 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def index
     @comments = @post.comments
-  end
-
-  private
-
-  def set_post
-    @post = Post.find params[:post_id]
   end
 
   def new
@@ -24,10 +19,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_comments_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_comments_path(@post)
+  end
+
   private
+
   def comment_params
     params.require(:comment).permit(:content)
   end
-end
 
+  def set_post
+    @post = Post.find params[:post_id]
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+end
 
